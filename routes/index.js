a889afc0746d322d8c913af25210e70007e9298c
jenobrock <jenovicnginamaut@gmail.com>
2025-05-08 14:37:35 +0200

    ...

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
 var route_users = require("./routes");
-app.use("/api", route_config);
+app.use("/api", route_users);
 
 module.exports = app;
