32cb1ffbbcfc47e2c39aa5fe6d278f8b5b7f7344
jenobrock <jenovicnginamaut@gmail.com>
2025-05-08 15:06:37 +0200

    ...

--- models/bus.js
+++ models/bus.js
@@ -5,6 +5,8 @@ const busSchema = new mongoose.Schema({
     numberPlate: { type: String, required: true, unique: true },
     capacity:    { type: Number, required: true },
     driver:      { type: mongoose.Schema.Types.ObjectId, ref: 'Chauffeur' },
+    controler:      { type: mongoose.Schema.Types.ObjectId, ref: 'Chauffeur' },
+    trajet:      { type: mongoose.Schema.Types.ObjectId, ref: 'Trajet' },
   });
   const Bus = mongoose.model('Bus', busSchema);
   module.exports = Bus;
