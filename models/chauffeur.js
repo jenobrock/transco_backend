ba71e29f7f6a43905fc57c3f270aeb016b60ac69
jenobrock <jenovicnginamaut@gmail.com>
2025-05-08 14:35:52 +0200

    ...

--- models/chauffeur.js
+++ models/chauffeur.js
@@ -3,8 +3,10 @@ const { Schema } = mongoose;
 
 const chauffeurSchema = new mongoose.Schema({
     name:          { type: String, required: true },
-    licenseNumber: { type: String, required: true, unique: true },
-    phone:         { type: String }
+    licenseNumber: { type: String },
+    phone:         { type: String },
+    password: { type: String  },
+    role: { type: String }, 
   });
   const Chauffeur = mongoose.model('Chauffeur', chauffeurSchema);
   
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
 var route_users = require("./routes");
+app.use("/api", route_config);
 
 module.exports = app;
